a317cd4ac0ec9e78288c5afcb5d286cf4b90c7a3
formotiis <nosvulca@gmail.com>
2017-12-07 11:49:39 +0100

    Backuped front page, made some sort of game thingy

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -6,16 +6,76 @@ include("header.php");
 <div class="body"></div>
 <script src="<?php echo base_url();?>js/register.js"></script>
 <div id="container">
-	<h1>Inscription</h1>
+	<h1>Jouer</h1>
 	<div class="contents">
 	<?php $this->load->helper('form');
 		  echo validation_errors();
 		  echo form_open('/inscription');?>
-		<div class="register">
-			Nom d'utilisateur: <br><input type="text" placeholder="Pseudo" name="pseudo" required><br>
-			Mot de passe: <br><div id="error"></div><input type="password" id="mdp" placeholder="Mot de passe" name="mdp" required><br>
-			Confirmer mot de passe:	<br><div id="cerror"></div><input type="password" id="cmdp" placeholder="Mot de passe" name="cmdp" required><br>
-			<input type="submit" id="register" value="Inscription"><br>
+		<div class="join"> <input type="submit" id="join" value="Rejoindre une Partie" style="display: none;"> </div>
+		<div class="game">
+			<div class="p1area">
+				<div class="pcInHand">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/1guard.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/1guard.png">
+				</div>
+				<div class="playedCard">
+					<img class="dispCard" id="xxx" value="visible" src="<?php echo base_url();?>img/8princess.png">
+				</div>
+				<div class="playername">
+					Yourself
+				</div>
+			</div>
+			<div class="p2area">
+				<div class="pcInHand">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/1guard.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/4handmaid.png">
+				</div>
+				<div class="playedCard">
+					<img class="dispCard" id="xxx" value="visible" src="<?php echo base_url();?>img/7countess.png">
+				</div>
+				<div class="playername">
+					Player 2
+				</div>
+			
+			</div>
+			<div class="p3area">
+				<div class="pcInHand">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/1guard.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/1guard.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/2priest.png">
+
+				</div>
+				<div class="playedCard">
+					<img class="dispCard" id="xxx" value="visible" src="<?php echo base_url();?>img/6king.png">
+				</div>
+				<div class="playername">
+					Player 3
+				</div>
+			</div>
+			<div class="p4area">
+				<div class="pcInHand" value="3">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/3baron.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/4handmaid.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/3baron.png">
+				</div>
+				<div class="playedCard">
+					<img class="dispCard" id="xxx" value="visible" src="<?php echo base_url();?>img/5prince.png">
+				</div>
+				<div class="playername">
+					Player 4
+				</div>
+			
+			</div>
+			<div class="nparea">
+				<div class="bannedCard">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/3baron.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/3baron.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/3baron.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/2priest.png">
+				</div>
+				<div class="thrownCards"></div>
+			
+			</div>
 		</div>
 	</form>
 	</div>
