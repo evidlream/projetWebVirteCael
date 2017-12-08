bcb3993bf7607b802ebac816fca9d3c85bd9eab4
formotiis <nosvulca@gmail.com>
2017-12-09 00:51:14 +0100

    views update.

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -4,6 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 include("header.php");
 ?>
 <div class="body"></div>
+<?php
+	include("sidemenu.php");
+?>
 <script src="<?php echo base_url();?>js/register.js"></script>
 <div id="container">
 	<h1>Jouer</h1>
@@ -17,9 +20,7 @@ include("header.php");
 				<div class="pcInHand">
 					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/1guard.png">
 					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/1guard.png">
-				</div>
-				<div class="playedCard">
-					<img class="dispCard" id="xxx" value="visible" src="<?php echo base_url();?>img/8princess.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/1guard.png">
 				</div>
 				<div class="playername">
 					Yourself
@@ -27,11 +28,9 @@ include("header.php");
 			</div>
 			<div class="p2area">
 				<div class="pcInHand">
-					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/1guard.png">
-					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/4handmaid.png">
-				</div>
-				<div class="playedCard">
-					<img class="dispCard" id="xxx" value="visible" src="<?php echo base_url();?>img/7countess.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/0back.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/0back.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/0back.png">
 				</div>
 				<div class="playername">
 					Player 2
@@ -40,41 +39,42 @@ include("header.php");
 			</div>
 			<div class="p3area">
 				<div class="pcInHand">
-					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/1guard.png">
-					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/1guard.png">
-					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/2priest.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/0back.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/0back.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/0back.png">
 
 				</div>
-				<div class="playedCard">
-					<img class="dispCard" id="xxx" value="visible" src="<?php echo base_url();?>img/6king.png">
-				</div>
+				
 				<div class="playername">
 					Player 3
 				</div>
 			</div>
 			<div class="p4area">
 				<div class="pcInHand" value="3">
-					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/3baron.png">
-					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/4handmaid.png">
-					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/3baron.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/0back.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/0back.png">
+					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/0back.png">
 				</div>
-				<div class="playedCard">
-					<img class="dispCard" id="xxx" value="visible" src="<?php echo base_url();?>img/5prince.png">
-				</div>
+
 				<div class="playername">
 					Player 4
 				</div>
 			
 			</div>
+			<div class="discardpile">
+					<img class="dispCard" id="xxx" value="visible" src="<?php echo base_url();?>img/9slot.png">
+			</div>
 			<div class="nparea">
+				
 				<div class="bannedCard">
 					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/3baron.png">
 					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/3baron.png">
 					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/3baron.png">
-					<img class="handCard" id="xxx" value="visible" src="<?php echo base_url();?>img/2priest.png">
+
 				</div>
-				<div class="thrownCards"></div>
-			
+				<div class="stackpile">
+				</div>
+			<script src="<?php echo base_url();?>js/playedCard.js"></script>
 			</div>
 		</div>
 	</form>
---
+++ application/views/sidemenu.php
@@ -0,0 +1,6 @@
+<div class="sidenav" id="side">
+	<div class="arrow"><i class="arrowup" onclick="changeCard(true)"></i></div>
+	<img class="displayCard" id="sideCard" src="<?php echo base_url();?>img/4handmaid.png">
+	<div class="arrowd"><i class="arrowdown" onclick="changeCard(false)"></i></div>
+	<script src="<?php echo base_url();?>js/sidemenu.js"></script>
+</div>
