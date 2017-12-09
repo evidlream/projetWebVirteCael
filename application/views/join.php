9153cd50793a7246b845da9697e2ff7de82fa44b
formotiis <nosvulca@gmail.com>
2017-12-09 21:34:16 +0100

    waiting and join views added

---
+++ application/views/join.php
@@ -0,0 +1,14 @@
+<div class="contents" style="text-align:center;">
+<form>
+	<input type="submit" id="join2p" value="Rejoindre Partie 2j">
+</form>
+
+<form>
+	<input type="submit" id="join3p" value="Rejoindre Partie 3j">
+</form>
+
+<form>
+	<input type="submit" id="join4p" value="Rejoindre Partie 4j">
+</form>
+
+</div>
---
+++ application/views/wait.php
@@ -0,0 +1,7 @@
+<div class="contents" style="text-align:center;">
+	Joueurs en attente pour ce type de partie:
+	</br>
+	<div id="waitlist" class="username" style="margin-top:10px;">
+		Yourself </br>
+	</div>
+</div>
