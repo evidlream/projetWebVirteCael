fa12ae375d5f01b216daaabc002cb583b25dd6db
evidlream <yann.cael4@etu.univ-lorraine.fr>
2017-12-10 20:14:01 +0100

    debut code ajax, attente joueur

--- application/controllers/Attente.php
+++ application/controllers/Attente.php
@@ -1,11 +1,21 @@
 <?php
 	class Attente extends CI_Controller {
 
+		public function __construct(){
+			parent::__construct();
+		}
+	
 		public function attenteJoueur($nombreJoueur)
 		{
 			session_start();
 			$this->load->helper(array('form','url'));
-			$this->load->view('wait',$nombreJoueur);
+			$data['nombreJoueur'] = $nombreJoueur;
+			$this->load->view('wait',$data);
+		}
+		
+		public function attenteJoueurPersistante($nb){
+
+			echo '0'."/".$nb;
 		}
 	
 	}
--- application/views/wait.php
+++ application/views/wait.php
@@ -2,12 +2,24 @@
 include('header.php');?>
 
 <div class="contents" style="text-align:center;">
-	Joueurs en attente pour ce type de partie:
+	Recherche de joueur en cours :
 	</br>
 	<div id="waitlist" class="username" style="margin-top:10px;">
-		Yourself </br>
+		1/<?php echo $nombreJoueur;?>
 	</div>
 </div>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js" type="text/javascript"></script>
+<script>
+
+$(document).ready(function(){
+    function ajax(){
+        $('#waitlist').load("<?php echo base_url(); ?>index.php/Attente/attenteJoueurPersistante/<?php echo $nombreJoueur?>");
+    }
+    setInterval(ajax, 5000);
+});
+
+</script>
 
 
 <?php include('footer.php');?>
